761b6916421d1b1c91607e23208b0958dd8db18c
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-29 17:27:20 +0300

    Add project files.

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	ptr = new int;
+//
+//	cout << "addres = " << ptr << "Value = " << *ptr << endl;
+//
+//	delete ptr;
+//
+//	return 0;
+//}
